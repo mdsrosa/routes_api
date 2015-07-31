eeb664538496ea0a23c58abb50e335e8a035c6b7
Matheus Rosa <matheusdsrosa@gmail.com>
2015-07-31 17:45:47 -0300

    Implements calculate-cost endpoint

--- lib/api.rb
+++ lib/api.rb
@@ -1,3 +1,5 @@
+require_relative './dijkstra/dijkstra'
+
 class WalmartChallengeAPI < Sinatra::Base
   get '/' do
     'Walmart Challenge API :)'
@@ -8,7 +10,24 @@ class WalmartChallengeAPI < Sinatra::Base
   end
 
   post '/routes/calculate-cost' do
-    response = {:cost => 6.25, :route => 'A B D'}
-    JSON.dump(response)
+    routes = []
+
+    # populating routes like a matrix for graph
+    Route.all.each { |r| routes.append [r.origin_point.to_sym,
+                                        r.destination_point.to_sym,
+                                        r.distance] }
+    # getting params
+    origin_point = params[:origin_point].to_sym
+    destination_point = params[:destination_point].to_sym
+    autonomy = params[:autonomy].to_f
+    fuel_price = params[:fuel_price].to_f
+
+    # calculating
+    g = Graph.new(routes)
+    path, dst = g.shortest_path(origin_point, destination_point)
+    route_path = path.join(" ")
+    cost = dst.to_f * fuel_price / autonomy
+
+    JSON.dump({ :cost => cost, :route => route_path})
   end
 end
