d27f129b5ffb11dc9aa3d5d9a1715f27efba65de
Matheus Rosa <matheusdsrosa@gmail.com>
2015-07-31 17:56:39 -0300

    Refactored api.rb

--- lib/api.rb
+++ lib/api.rb
@@ -1,24 +1,31 @@
+module WalmartChallengeAPIModule
+  module Helpers
+    def routes_matrix
+      routes = []
+      # populating routes like a matrix for graph
+      Route.all.each { |r| routes.append [r.origin_point,
+                                          r.destination_point,
+                                          r.distance] }
+      routes
+    end
+
+    def calculate_shortest_path(start, stop)
+      require_relative './dijkstra/dijkstra'
+      g = Graph.new(routes_matrix)
+      path, dst = g.shortest_path(start, stop)
+    end
+
+    def calculate_cost(distance, autonomy, fuel_price)
+      distance * fuel_price / autonomy
+    end
+  end
+end
+
 class WalmartChallengeAPI < Sinatra::Base
-  def routes_matrix
-    routes = []
-    # populating routes like a matrix for graph
-    Route.all.each { |r| routes.append [r.origin_point,
-                                        r.destination_point,
-                                        r.distance] }
-    routes
-  end
+  include WalmartChallengeAPIModule::Helpers
 
-  def calculate_shortest_path(start, stop)
-    require_relative './dijkstra/dijkstra'
-    g = Graph.new(routes_matrix)
-    path, dst = g.shortest_path(start, stop)
-  end
-
-  def calculate_cost(distance, autonomy, fuel_price)
-    distance * fuel_price / autonomy
-  end
   get '/' do
-    'Walmart Challenge API :)'
+      'Walmart Challenge API :)'
   end
 
   get '/routes' do
