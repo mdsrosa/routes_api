79dee96dfde5716f5482e468578176c60edfbfd4
Matheus Rosa <matheusdsrosa@gmail.com>
2015-08-02 16:58:43 -0300

    Update config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,14 +4,7 @@ require './lib/db_config'
 
 require 'yaml'
 environment = ENV.fetch('RACK_ENV') { 'development' }
-config = DBConfig.new(environment).options
-
-# for heroku
-if environment == 'production'
-  config = YAML.load(ERB.new(File.read(File.join('config', 'database.yml'))).result)
-  config = config[environment]
-end
-
+config = YAML.load(ERB.new(File.read(File.join('config', 'database.yml'))).result)[environment]
 ActiveRecord::Base.establish_connection(config)
 
 require 'routes_api'
